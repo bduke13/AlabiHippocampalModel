ed4777a392982e02748d100a9771f7da95548188
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-10-15 18:15:22 -0400

    added controller back in

---
+++ controllers/bek_controller/controller.py
@@ -0,0 +1,38 @@
+"""my_controller_iCreate controller."""
+
+from driver import Driver
+import numpy as np
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+np.random.seed(0)
+bot = Driver()
+
+
+def run_bot(stage, randomize_start_loc):
+    if stage not in [1, 2, 3, 4, 5]:
+        raise ValueError("Invalid mode. Choose either '1', '2', or '3'")
+    if stage == 1:
+        bot.clear()
+        bot.initialization(mode="learning", randomize_start_loc=randomize_start_loc)
+        bot.run("explore")
+    elif stage == 2:
+        bot.initialization(mode="dmtp", randomize_start_loc=randomize_start_loc)
+        bot.run("explore")
+    elif stage == 3:
+        bot.initialization(mode="dmtp", randomize_start_loc=randomize_start_loc)
+        bot.run("exploit")
+    elif stage == 4:  # plotting mode
+        bot.initialization(mode="explore", randomize_start_loc=randomize_start_loc)
+        bot.run("explore")
+    elif stage == 5:  # teleport and record mode
+        bot.initialization(mode="explore", randomize_start_loc=randomize_start_loc)
+        bot.run("teleport")
+
+
+run_bot(3, randomize_start_loc=True)
+
+# 1 learning, explore (RUN FOR ABOUT 2 HRS SIMULATION TIME, OR OBSERVE BVC-PCV WEIGHTS)
+# 2 dmtp, explore (RUN ONCE)
+# 3 dmtp, exploit
