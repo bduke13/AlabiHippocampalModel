e978ff27079ce39084990a543e5ac51902b4f935
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-11-18 12:17:12 -0500

    updated world with new range finder as well as driver to get the data

--- controllers/bek_controller/bek_controller.py
+++ controllers/bek_controller/bek_controller.py
@@ -9,7 +9,7 @@ bot = Driver()
 # 4. (optional) PLOTTING
 
 bot.initialization(
-    mode=RobotMode.EXPLOIT,
+    mode=RobotMode.LEARN_OJAS,
     randomize_start_loc=True,
     run_time_hours=2,
     start_loc=[4, -4],
