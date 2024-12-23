2fdc299ea62f319597407822d0402197f3ea0fb7
bduke13 <dukenbbs@mail.uc.edu>
2024-12-22 20:45:34 -0500

    controller update

--- controllers/bek_controller/bek_controller.py
+++ controllers/bek_controller/bek_controller.py
@@ -1,17 +1,23 @@
 """my_controller_iCreate controller."""
 
 from driver import Driver, RobotMode
+from analysis.stats_collector import stats_collector
 
-bot = Driver()
 # 1. LEARN_OJAS
 # 2. DMTP
 # 3. EXPLOIT
 # 4. (optional) PLOTTING
 
+bot = Driver()
+stats_collector = stats_collector(output_dir="controllers\\bek_controller\\analysis\\stats")
+
 bot.initialization(
-    mode=RobotMode.EXPLOIT,
+    mode=RobotMode.LEARN_OJAS,
     randomize_start_loc=True,
     run_time_hours=2,
-    start_loc=[4, -4],
+    dmtp_run_time_hours=0.5,
+    start_loc=[2, -2],
+    enable_multiscale=True,
+    stats_collector=stats_collector
 )
 bot.run()
