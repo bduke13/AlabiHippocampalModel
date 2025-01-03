b27339413f7f2f49c8371138717a8a801e5f622c
bduke13 <dukenbbs@mail.uc.edu>
2025-01-03 01:32:15 -0500

    more controller changes

--- controllers/bek_controller/bek_controller.py
+++ controllers/bek_controller/bek_controller.py
@@ -4,9 +4,6 @@ import os
 from driver import Driver, RobotMode
 from analysis.stats_collector import stats_collector
 
-# Define the mode to run
-SELECTED_MODE = "EXPLOIT_SAVE"  # Options: LEARN_OJAS, DMTP, EXPLOIT, EXPLOIT_SAVE, PLOTTING
-
 # Function to detect the current world name dynamically
 def get_world_name(bot):
     world_name = bot.getWorldPath()  # This returns the full path
@@ -14,24 +11,20 @@ def get_world_name(bot):
     return world_name
 
 # Function to handle LEARN_OJAS or DMTP modes
-def run_learn_or_dmtp(mode, start_loc, run_time_hours=1, dmtp_run_time_hours=0.2, enable_multiscale=False):
+def run_learn_or_dmtp(mode, run_time_hours, start_loc, randomize_start_loc, enable_multiscale):
     print(f"Running in mode: {mode.name}")
     bot = Driver()
-
-    stats_collector_instance = stats_collector(output_dir="controllers\\bek_controller\\analysis\\stats")
     bot.initialization(
         mode=mode,
-        randomize_start_loc=False,
         run_time_hours=run_time_hours,
-        dmtp_run_time_hours=dmtp_run_time_hours,
         start_loc=start_loc,
+        randomize_start_loc=randomize_start_loc,
         enable_multiscale=enable_multiscale,
-        stats_collector=stats_collector_instance
     )
     bot.run()
 
 # Function to handle EXPLOIT modes
-def run_exploit(corners, save_data=False):
+def run_exploit(corners, enable_multiscale, save_data=False):
     bot = Driver()
     bot.trial_indices = {}
 
@@ -41,7 +34,10 @@ def run_exploit(corners, save_data=False):
 
     # Define the stats folder path based on the world name
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    stats_folder = os.path.join(current_dir, "analysis", "stats", world_name)
+    if enable_multiscale:
+        stats_folder = os.path.join(current_dir, "analysis", "stats", "multiscale", world_name, "JSON")
+    else:
+        stats_folder = os.path.join(current_dir, "analysis", "stats", "vanilla", world_name, "JSON")
     os.makedirs(stats_folder, exist_ok=True)
 
     for corner in corners:
@@ -72,22 +68,39 @@ def run_exploit(corners, save_data=False):
             bot.run()
             bot.simulationReset()
 
+    bot.simulationSetMode(bot.SIMULATION_MODE_PAUSE)
+
+################################# START HERE ##################################
+    
+# Define the mode to run
+SELECTED_MODE = "EXPLOIT_SAVE"  # Options: LEARN_OJAS, DMTP, EXPLOIT, EXPLOIT_SAVE, PLOTTING
+run_time_hours = 1
+start_loc = [2, 2]
+randomize_start_loc = True
+enable_multiscale = True
+
+corners = [[2, -2], [-2, -2], [2, 2], [-2, 2]]
+
 # Main function
 if __name__ == "__main__":
 
-    if SELECTED_MODE == "LEARN_OJAS":  # LEARN_OJAS
-        run_learn_or_dmtp(mode=RobotMode.LEARN_OJAS, start_loc=[2, 2])
-    elif SELECTED_MODE == "DMTP":  # DMTP
-        run_learn_or_dmtp(mode=RobotMode.DMTP, start_loc=[2, 2], dmtp_run_time_hours=0.2, enable_multiscale=True)
+    if SELECTED_MODE == "LEARN_OJAS":
+        run_learn_or_dmtp(mode=RobotMode.LEARN_OJAS, 
+                    run_time_hours=run_time_hours,
+                    start_loc=corners[1], 
+                    randomize_start_loc=randomize_start_loc,
+                    enable_multiscale=enable_multiscale)
+    if SELECTED_MODE == "DMTP":  # LEARN_OJAS
+        run_learn_or_dmtp(mode=RobotMode.DMTP, 
+                          run_time_hours=run_time_hours,
+                          start_loc=corners[3], 
+                          randomize_start_loc=False,
+                          enable_multiscale=enable_multiscale)
     elif SELECTED_MODE == "EXPLOIT":  # Non-data-saving EXPLOIT
-        # Define the corners
-        corners = [[2, -2], [-2, -2], [2, 2], [-2, 2]]
-        run_exploit(corners, save_data=False)
+        run_exploit(corners, enable_multiscale, save_data=False)
     elif SELECTED_MODE == "EXPLOIT_SAVE":  # Data-saving EXPLOIT
-        # Define the corners
-        corners = [[2, -2], [-2, -2], [2, 2], [-2, 2]]
-        run_exploit(corners, save_data=True)
+        run_exploit(corners, enable_multiscale, save_data=True)
     elif SELECTED_MODE == "PLOTTING":  # PLOTTING
-        run_learn_or_dmtp(mode=RobotMode.PLOTTING, start_loc=[2, 2])
+        run_learn_or_dmtp(mode=RobotMode.PLOTTING, start_loc=start_loc, enable_multiscale=enable_multiscale)
     else:
         print("Invalid mode selected.")
